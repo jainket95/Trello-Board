16f43537f47109a974d1d7c7b791b0715eb5a24c
thelucidrealist <48476821+thelucidrealist@users.noreply.github.com>
2019-06-15 19:29:44 +0530

    Complete Project with some errors

--- Trello Project/app.js
+++ Trello Project/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init(){
+
 //alert('connected');
 function addNewItem(){
   var newItem = document.getElementById('addinput');
@@ -8,7 +9,7 @@ function addNewItem(){
   var addItemList = document.getElementById('addlisthere');
 
   button.addEventListener('click',function(e){
-    e.preventDefault();
+    e.stopPropagation();
     var newItemData = newItem.value;
     var li = document.createElement('li');
     
@@ -44,7 +45,7 @@ function addNewItem(){
     li.appendChild(spanDeleted);
     console.log(li);
 
-    li.className = 'ul#addlisthere li';
+    li.className = 'ul#addlisthere li addli';
     li.appendChild(document.createTextNode(newItemData));
     addItemList.appendChild(li);
     newItem.textContent ='';
@@ -57,52 +58,156 @@ function toggleInputDisplay(){
 
   var form = document.getElementById('myform');
   //form.style.display = 'none';
+  
   showInputDisplay[0].addEventListener('click',function(){
-    if(form.style.display == 'none'){
-      form.style.display = 'block';
-    } else {
+    form.style.display == 'none' ?
+      form.style.display = 'block' :
       form.style.display = 'none';
-    }
   });
 }
-
 toggleInputDisplay();
 
 function addTogTaskList(){
-  var addTogList = document.querySelectorAll('.fa-pencil-square-o');
+  var addTogList = document.querySelectorAll('span#ongoing');
   console.log(addTogList);
 
-  addTogList.forEach(function(e){
-    console.log(e);
+  // addTogList.forEach(function(e){
+  //   console.log(e);
+  // })
+
+  addTogList.forEach(() => {
+      this.addEventListener('click',(e) => {
+        e.stopPropagation();
+        if(e.target.classList.contains('fa-pencil-square-o')){
+          var temp = e.target.parentElement.parentElement;
+          var removeOng = temp.firstElementChild;  
+          if(temp){
+            temp.removeChild(removeOng);
+          }
+
+          temp.classList.contains('addli') ? 
+          temp.classList.remove('addli') :
+          temp.classList.add('ongli');
+
+          temp.classList.contains('ul#addlisthere li') ?
+          temp.classList.remove('ul#addlisthere','li') :
+          temp.classList.add('ul#addogtask','li');
+
+          var tempClone = temp.cloneNode(true);
+          var addTogListItem = document.getElementById('addogtask');
+          addTogListItem.appendChild(tempClone);
+          var temp = temp.parentElement.removeChild(temp); 
+        }
+      });
   })
-
-  for(var i=0; i < addTogList.length ; i++){
-    addTogList[i].addEventListener('click', function(e){
-      if(e.target.classList.contains('fa-pencil-square-o')){
-        var temp = e.target.parentElement.parentElement;
-        
-        console.log('tog function fired.');
-        console.log(temp);
-
-        var cloned = temp.cloneNode(true);
-        console.log(cloned);
-        
-        var addTogListItem = document.getElementById('addogtask');
-        console.log(addTogListItem);
-        addTogListItem.appendChild(cloned);
-        var temp = temp.parentElement.removeChild(temp); 
-        
-      }
-    })
- 
-  }
-
-  
-  
 }
 addTogTaskList();
 
+function addDoneTaskList(){
+  var addDoneList = document.querySelectorAll('span#done');
+  console.log(addDoneList);
 
+  // addTogList.forEach(function(e){
+  //   console.log(e);
+  // })
+
+  addDoneList.forEach(() => {
+      this.addEventListener('click', (e) => {
+        e.stopPropagation();
+        if(e.target.classList.contains('fa-check-square-o')){
+          var temp = e.target.parentElement.parentElement;
+          //console.log(temp);
+          var removeOng = temp.children[0];  
+          var removeDone = temp.children[1];  
+  
+          //console.log(removeOng);
+          //console.log(removeDone);
+          temp.classList.contains('ul#addlisthere li') ? 
+          temp.classList.remove('ul#addlisthere','li') :
+          temp.classList.contains('ul#addogtask li' )?
+          temp.classList.remove('ul#addogtask','li'):
+          temp.classList.add('ul#addcomphere','li');
+
+          temp.classList.contains('addli') ?
+            temp.classList.remove('addli') :
+            temp.classList.remove('ongli');
+
+            temp.removeChild(removeOng);
+            temp.removeChild(removeDone);
+          var tempClone = temp.cloneNode(true);
+          var addDoneListItem = document.getElementById('addcomphere');
+          //console.log(addDoneListItem);
+          addDoneListItem.appendChild(tempClone);
+          var temp = temp.parentElement.removeChild(temp); 
+        }
+      });
+   
+  })
+}
+addDoneTaskList();
+
+function deleteTaskList(){
+  var deleteList = document.querySelectorAll("span#delete");
+  //console.log(deleteList);
+
+  //deleteList.forEach((e)=>console.log(e));
+
+  deleteList.forEach((i) => {
+    this.addEventListener('mouseup',(e) => {
+      e.stopPropagation();
+      var temp = e.target;
+      if(temp.classList.contains('fa-trash')){
+          var tempDel = temp.parentElement.parentElement;
+          //console.log(temp);
+           tempDel.parentElement.removeChild(tempDel);
+           //console.log(removeDeleteItem);
+      }
+    })
+  })
+}
+deleteTaskList();
+
+function selectOff(){
+var addSelectOffList = document.querySelectorAll('ul#addlisthere');
+var ongSelectedOffList = document.querySelectorAll('ul#addogtask');
+
+ addSelectOffList.forEach((e => console.log(e)));
+ ongSelectedOffList.forEach((e => console.log(e)));
+
+addSelectOffList.forEach(()=> {
+  this.addEventListener('mouseup',(e) => {
+    e.stopPropagation();
+    var temp = e.target; 
+    if(temp.classList.contains('addli ') ){
+        temp.classList.add('addselected');
+      } else if (temp.classList.contains('addli addselected')) {
+        temp.classList.remove('addselected');
+      } else {
+        return;
+      }
+  })
+})
+
+ongSelectedOffList.forEach(()=> {
+  this.addEventListener('mouseup',(e) => {
+    e.stopPropagation();
+    var temp = e.target;
+    //console.log(temp);
+      if(temp.classList.contains('ongli')){
+        console.log('fired');
+        if(temp.classList.contains('ongselected')){
+          console.log('still firing');
+            temp.classList.add('ongselected');
+           } else { //if(temp.classList.contains('ongli ongselected')){
+            temp.classList.remove('ongselected');
+          }
+      }
+   })
+        
+})
+
+}
+selectOff();
 
 }
 
